91b3b972fd23e02d4875a1f1a0d51fd919afdd4f
cdpyx <2079390238@qq.com>
2025-03-08 12:36:15 +0800

    1

--- src/main/java/cn/zbx1425/resourcepackupdater/mixin/MinecraftMixin.java
+++ src/main/java/cn/zbx1425/resourcepackupdater/mixin/MinecraftMixin.java
@@ -7,12 +7,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.CompletableFuture;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
 
-    @Inject(at = @At("HEAD"), method = "reloadResources()Ljava/util/concurrent/CompletableFuture;")
-    private void onReloadResources(CallbackInfo ci) {
+    @Inject(at = @At("HEAD"), method = "reloadResourcePacks(Z)Ljava/util/concurrent/CompletableFuture;")
+    private void onReloadResourcePacks(boolean bl, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
         ResourcePackUpdater.dispatchSyncWork();
         ResourcePackUpdater.modifyPackList();
     }
